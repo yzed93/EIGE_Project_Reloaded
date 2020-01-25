ae6a90a73846b6b0bb57d682bde041e94c1b3e59
Dennis <dennis.wiredu@outlook.de>
2020-01-25 16:34:56 +0100

    Speer Script

---
+++ EIGE RE/Assets/Scripts/SpearThrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearThrow : MonoBehaviour
+{
+   
+    public Rigidbody speer;
+    [SerializeField]
+    [Range(10, 50)]
+    public float throwForce;
+    
+
+     void Awake()
+    {
+        speer.transform.parent = null;
+        speer.isKinematic = false;
+        speer.AddForce(Camera.main.transform.TransformDirection(Vector3.forward) * throwForce, ForceMode.Impulse);
+        speer.AddTorque(speer.transform.TransformDirection(Vector3.up) * 100, ForceMode.Impulse);
+    }
+
+
+}
