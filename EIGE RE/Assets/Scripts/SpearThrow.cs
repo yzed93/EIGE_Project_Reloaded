480a8f4a6ceb2b7bc55720859d66812cca801b17
Dennis <dennis.wiredu@outlook.de>
2020-01-25 23:31:09 +0100

    Gute Nacht

--- EIGE RE/Assets/Scripts/SpearThrow.cs
+++ EIGE RE/Assets/Scripts/SpearThrow.cs
@@ -13,36 +13,41 @@ public class SpearThrow : MonoBehaviour
     public bool bewaffnet;
     
 
+    
+
      void Update()
     {
         if (jarid.getActionState()==ActionState.AIMING && Input.GetButtonDown("Fire1") && bewaffnet == true)
         {
             
             Throw();
-            SpearSpawn();
+            
         }
     }
-    void SpearSpawn()
+    /*void SpearSpawn()
     {
+       
+        Rigidbody neuerSpeer = Instantiate(speer, new Vector3(0.6281128f, -0.077f, -1.795f), speer.transform.rotation);
+        neuerSpeer.isKinematic = true;
+        
+       neuerSpeer.transform.parent = waffenSpawn;
         Debug.Log("Spawn geht");
-        Rigidbody neuerSpeer = new Rigidbody();
-        neuerSpeer = Instantiate(speer, waffenSpawn);
-    
-        
-        
+        speer = neuerSpeer;
+
 
     }
+    */
     void Throw()
     {
        
         {
-
-
             speer.transform.parent = null;
             speer.isKinematic = false;
             speer.AddForce(Camera.main.transform.TransformDirection(Vector3.forward) * throwForce, ForceMode.Impulse);
             speer.AddTorque(speer.transform.TransformDirection(Vector3.up) * 100, ForceMode.Impulse);
             bewaffnet = false;
+            
+            
         }
     }
 
